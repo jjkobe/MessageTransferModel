5bf9d14b6c719bfbb59e59b479e0b1614b5d8feb
Chris <tjuyinsh@gmail.com>
2016-03-24 12:50:30 +0800

    Conflict fixed

--- Message/src/client/client.java
+++ Message/src/client/client.java
@@ -1,13 +1,8 @@
 package client;
 
-<<<<<<< HEAD
 import UI.SendMsg;
-
 import java.awt.*;
-=======
 import server.Server;
-
->>>>>>> dev
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -18,22 +13,19 @@ import java.net.UnknownHostException;
 
 import javax.swing.*;
 
-<<<<<<< HEAD
-public class client extends JFrame implements ActionListener{
-=======
-public class Client extends JFrame{
+
+public class Client extends JFrame implements ActionListener{
     private JLabel jLabel1;
->>>>>>> dev
     private JTextField usertext;
     private JPasswordField passtext;
     private JTextField ipadddr;
     private JLabel hint;
-    public static client cli;
+    public static Client cli;
     private static FileWriteThread fileWriteThread=new FileWriteThread();
     String name;
     String password;
-<<<<<<< HEAD
-    public client() {
+
+    public Client() {
         Container conn = this.getContentPane();//加入一个容器
 
         conn.setLayout(null);//绝对布局
@@ -126,115 +118,12 @@ public class Client extends JFrame{
         this.setDefaultCloseOperation(3);//当点击关闭时进行关闭
 
         this.setVisible(true);
-=======
-    public Client() {
-        {
-            getContentPane().setLayout(null);
-        }
-        {
-            jLabel1 = new JLabel();
-            getContentPane().add(jLabel1);
-            jLabel1.setText("\u7528\u6237\u540d");
-            jLabel1.setBounds(39, 39, 63, 18);
-        }
-        {
-            usertext = new JTextField();
-            getContentPane().add(usertext);
-            usertext.setBounds(109, 37, 156, 22);
-        }
-        {
-            jLabel2 = new JLabel();
-            getContentPane().add(jLabel2);
-            jLabel2.setText("\u5bc6\u7801");
-            jLabel2.setBounds(39, 77, 38, 18);
-        }
-        {
-            passtext = new JTextField();
-            getContentPane().add(passtext);
-            passtext.setBounds(109, 75, 156, 22);
-        }
-        {
-            loginbutton = new JButton();
-            getContentPane().add(loginbutton);
-            loginbutton.setText("\u767b\u9646");
-            loginbutton.setBounds(90, 113, 91, 28);
-            loginbutton.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent e)
-                {
-                    name=usertext.getText().trim();
-                    password=passtext.getText().trim();
-                    if(password.equals(""))
-                    {
-                        password="";
-                    }
-                    if(name!=null&&name.length()>0)
-                    {
-                        hint.setText("正在验证客户端，请稍后......");
-                        start();
-                    }
-                }
 
-                public  void start() {
-
-                    new Thread(new Runnable(){
-
-                        public void run() {
-                            try {
-                                Socket s=new Socket("127.0.0.1", Server.SERVER_PORT);
-                                OutputStream os;
-
-                                InputStream is;
-                                os=s.getOutputStream();
-                                os.write(name.getBytes());
-                                os.write(Server.ACCOUNT_SPLIT_TAG);
-                                os.write(password.getBytes());
-                                os.flush();
-                                Thread.sleep(1000);
-                                is=s.getInputStream();
-                                int len=is.available();
-                                System.out.println("length: "+len);
-                                byte[] bytes=new byte[len];
-                                is.read(bytes);
-                                String result=new String(bytes);
-                                System.out.println("result: "+result);
-                                if(result.equals(Server.ACK))
-                                {
-                                    hint.setText("验证成功，欢迎光临");
-                                    fileWriteThread.addContent(1);
-                                }else{
-                                    passtext.setText(null);
-                                    hint.setText("用户名或密码有误，请重新输入");
-                                    fileWriteThread.addContent(0);
-                                }
-                            } catch (UnknownHostException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                    }).start();
-                }
-            });
-        }
-        {
-            hint = new JLabel();
-            getContentPane().add(hint);
-            hint.setBounds(90, 8, 172, 23);
-        }
-        {
-            this.setSize(318, 188);
-            setVisible(true);
-        }
->>>>>>> dev
     }
 
     public static void main(String[] args) {
         fileWriteThread.start();
-<<<<<<< HEAD
-        cli=new client();
+        cli=new Client();
     }
 
     @Override
@@ -258,9 +147,6 @@ public class Client extends JFrame{
             this.dispose();
 
         }
-=======
-        new Client();
->>>>>>> dev
     }
 
     public  void start() {
@@ -269,13 +155,13 @@ public class Client extends JFrame{
 
             public void run() {
                 try {
-                    Socket s=new Socket("10.0.1.6",5555);
+                    Socket s=new Socket("127.0.0.1",Server.SERVER_PORT);
                     OutputStream os;
 
                     InputStream is;
                     os=s.getOutputStream();
                     os.write(name.getBytes());
-                    os.write('|');
+                    os.write(Server.ACCOUNT_SPLIT_TAG);
                     os.write(password.getBytes());
                     os.flush();
                     Thread.sleep(1000);
@@ -286,7 +172,7 @@ public class Client extends JFrame{
                     is.read(bytes);
                     String result=new String(bytes);
                     System.out.println("result: "+result);
-                    if(result.equals("ack"))
+                    if(result.equals(Server.ACK))
                     {
                         hint.setText("验证成功，欢迎光临");
                         fileWriteThread.addContent(1);
@@ -298,13 +184,10 @@ public class Client extends JFrame{
                         fileWriteThread.addContent(0);
                     }
                 } catch (UnknownHostException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
